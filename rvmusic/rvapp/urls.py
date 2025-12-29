cff8bb483a8033d24e3c2a166e9f840eec1dd379
Rijo007 <rijovarghese225@gmail.com>
2025-12-29 20:00:21 +0530

    updated template and view

--- rvmusic/rvapp/urls.py
+++ rvmusic/rvapp/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path
-from rvapp.views import home,about
+from rvapp.views import home,about,service
 
 urlpatterns = [
     path('',home,name='home'),
-    path('about',about,name='about')
+    path('about',about,name='about'),
+    path('service',service,name='service')
 ]
--- rvmusic/rvapp/views.py
+++ rvmusic/rvapp/views.py
@@ -4,8 +4,14 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request,'home.html')
+    return render(request,'index.html')
 
 
 def about(request):
     return render(request,'about.html')
+
+
+def service(request):
+    return render(request,'service.html')
+
+
